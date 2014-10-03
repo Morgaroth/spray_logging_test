5e86bb6b3696f6377d99c9ec81e9063c9312daa1
Mateusz Jaje <mateusz.jaje@sigmapoint.co>
2014-10-03 15:18:07 +0200

    added csv reporter

--- src/main/scala/example/stack/MetricsTrait.scala
+++ src/main/scala/example/stack/MetricsTrait.scala
@@ -1,7 +1,10 @@
 package example.stack
 
+import java.io.File
+import java.util.Locale
 import java.util.concurrent.TimeUnit
 
+import com.codahale.metrics.CsvReporter
 import com.codahale.metrics.ganglia.GangliaReporter
 import info.ganglia.gmetric4j.gmetric.GMetric
 import info.ganglia.gmetric4j.gmetric.GMetric.UDPAddressingMode
@@ -11,7 +14,13 @@ object Metrics {
   val metricRegistry = new com.codahale.metrics.MetricRegistry()
   val gangliaServer = new GMetric("192.168.56.110", 8649, UDPAddressingMode.UNICAST, 1)
   val reporter = GangliaReporter.forRegistry(metricRegistry).convertRatesTo(TimeUnit.SECONDS).convertDurationsTo(TimeUnit.MILLISECONDS).build(gangliaServer)
-  reporter.start(15,TimeUnit.SECONDS)
+  reporter.start(15, TimeUnit.SECONDS)
+  val csvReporter = CsvReporter.forRegistry(metricRegistry)
+    .formatFor(Locale.forLanguageTag("PL"))
+    .convertRatesTo(TimeUnit.SECONDS)
+    .convertDurationsTo(TimeUnit.MILLISECONDS)
+    .build(new File("~/projects/data/"))
+  csvReporter.start(10, TimeUnit.SECONDS)
 }
 
 trait Instrumented extends nl.grons.metrics.scala.InstrumentedBuilder {
