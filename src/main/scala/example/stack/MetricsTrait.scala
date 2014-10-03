080ac19bf496d0bf68255227a71e710147c0c67b
Mateusz Jaje <mateusz.jaje@sigmapoint.co>
2014-10-03 14:56:28 +0200

    updated ganglia client

--- src/main/scala/example/stack/MetricsTrait.scala
+++ src/main/scala/example/stack/MetricsTrait.scala
@@ -1,17 +1,20 @@
 package example.stack
 
+import java.util.concurrent.TimeUnit
+
+import com.codahale.metrics.ganglia.GangliaReporter
 import info.ganglia.gmetric4j.gmetric.GMetric
 import info.ganglia.gmetric4j.gmetric.GMetric.UDPAddressingMode
 
 
 object Metrics {
-
   val metricRegistry = new com.codahale.metrics.MetricRegistry()
-  val gangliaReporter = new GMetric("192.168.56.110", 8649, UDPAddressingMode.UNICAST, 1)
+  val gangliaServer = new GMetric("192.168.56.110", 8649, UDPAddressingMode.UNICAST, 1)
+  val reporter = GangliaReporter.forRegistry(metricRegistry).convertRatesTo(TimeUnit.SECONDS).convertDurationsTo(TimeUnit.MILLISECONDS).build(gangliaServer)
+  reporter.start(15,TimeUnit.SECONDS)
 }
 
 trait Instrumented extends nl.grons.metrics.scala.InstrumentedBuilder {
-
   val metricRegistry = Metrics.metricRegistry
 
 }
