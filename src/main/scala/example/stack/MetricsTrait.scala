e9c1907581dbbf6849fcd60785a7e5e25a4928d3
Mateusz Jaje <mateusz.jaje@sigmapoint.co>
2014-10-03 16:06:27 +0200

    added ganglia

--- src/main/scala/example/stack/MetricsTrait.scala
+++ src/main/scala/example/stack/MetricsTrait.scala
@@ -13,7 +13,7 @@ import info.ganglia.gmetric4j.gmetric.GMetric.UDPAddressingMode
 object Metrics {
   val metricRegistry = new com.codahale.metrics.MetricRegistry()
 
-  val gangliaServer = new GMetric("192.168.56.110", 8649, UDPAddressingMode.UNICAST, 1, true)
+  val gangliaServer = new GMetric("192.168.56.111", 8649, UDPAddressingMode.UNICAST, 1, true)
   val reporter = GangliaReporter.forRegistry(metricRegistry).convertRatesTo(TimeUnit.SECONDS).convertDurationsTo(TimeUnit.MILLISECONDS).build(gangliaServer)
   reporter.start(15, TimeUnit.SECONDS)
 
